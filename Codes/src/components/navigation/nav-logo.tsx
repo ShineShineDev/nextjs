fd7cd6b9a5ff5b1316adf53cc0ff663cb754bc2f
ShineShineDev <aungshine194@gmail.com>
2025-04-07 20:44:10 +0630

    nav

---
+++ Codes/src/components/navigation/nav-logo.tsx
@@ -0,0 +1,13 @@
+
+import Link from "next/link";
+
+const  NavLogo = async () =>{
+    
+    return (
+        <div>
+           <Link href={'/'} className="text-3xl font-bold text-primary" > Nav Logo</Link>
+        </div>
+    )
+}
+
+export default NavLogo;
